ad627ee0ae1836e86e64d0315ac665d8431dffd2
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-06 14:32:59 +0100

    ciclo for

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 // console.log("JS OK");
 
-fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data.response);
-  });
+for (i = 0; i < 10; i++) {
+  fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.response);
+    });
+}
