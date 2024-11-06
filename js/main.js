15ca598939a4c50d3d252e8214d1198291eedfb7
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-06 14:47:16 +0100

    test data in array

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 // console.log("JS OK");
 
+let emailsArray = [];
+
 for (i = 0; i < 10; i++) {
   fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.response);
+      const emailsItems = data.response;
+      emailsArray.push(emailsItems);
+      console.log(emailsArray);
+      //   console.log(data.response);
     });
 }
