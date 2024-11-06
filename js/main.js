01016ed54324f79ad0000b4606f8368c6672c493
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-06 14:29:18 +0100

    console log API mail

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log("JS OK");
+// console.log("JS OK");
+
+fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.response);
+  });
